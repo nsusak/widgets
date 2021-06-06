0dee204c186e1365c7187790631e77da9cde5084
Nikolina Šušak <nsusak0@gmail.com>
2021-06-06 22:45:05 +0200

    added a dropdown widget

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion"
+import Dropdown from "./components/Dropdown";
 import Search from "./components/Search";
 
 const items = [
@@ -17,11 +18,32 @@ const items = [
     }
 ];
 
+const options = [
+    {
+        label: "The Color Red",
+        value: "red"
+    },
+    {
+        label: "The Color Green",
+        value: "green"
+    },
+    {
+        label: "A Shade of Blue",
+        value: "blue"
+    }
+];
+
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
     return (
         <div>
             {/* <Accordion items={items} /> */}
-            <Search />
+            {/* <Search /> */}
+            <Dropdown
+                selected={selected}
+                onSelectedChange={setSelected}
+                options={options}
+            />
         </div>
     );
 };
