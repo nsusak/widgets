934a1531db74c6f4c841a57ca1bcf6a853a703f0
Nikolina Šušak <nsusak0@gmail.com>
2021-06-08 21:37:49 +0200

    debouncing on translate requests

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -4,11 +4,21 @@ import axios from "axios";
 
 const Convert = ({ language, text }) => {
     const [translated, setTranslated] = useState("");
+    const [debouncedText, setDebouncedText] = useState(text);
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedText(text)
+        }, 500);
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [text]);
+
     useEffect(() => {
         const doTranslation = async () => {
             const { data } = await axios.post("https://translation.googleapis.com/language/translate/v2", {}, {
                 params: {
-                    q: text,
+                    q: debouncedText,
                     target: language.value,
                     key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM"
                 }
@@ -17,7 +27,7 @@ const Convert = ({ language, text }) => {
             setTranslated(data.data.translations[0].translatedText);
         };
         doTranslation();
-    }, [language, text]);
+    }, [language, debouncedText]);
     return (
         <div>
             <h1 className="ui header">{translated}</h1>
